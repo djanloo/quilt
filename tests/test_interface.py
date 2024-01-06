bef5efe93bc4f4ac12721c9a8408193c0b8bbee8
djanloo <zzibecu@gmail.com>
2024-01-06 16:22:11 +0100

    checkpoint: everything works fine

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -32,48 +32,48 @@ if __name__=="__main__":
     spikenet = SpikingNetwork.from_yaml("spiking.yaml")
     spikenet.populations["Albert"].add_injector(200, 0, 100.0)
     spikenet.populations["Albert"].monitorize_spikes()
-    spikenet.populations["Albert"].monitorize_states()
-    spikenet.populations["MonaLisa"].monitorize_spikes()
-    spikenet.populations["MonaLisa"].monitorize_states()
+    # spikenet.populations["Albert"].monitorize_states()
+    # spikenet.populations["MonaLisa"].monitorize_spikes()
+    # spikenet.populations["MonaLisa"].monitorize_states()
 
     spikenet.interface.run(dt=0.1, time=200)
 
     albert_spikes = spikenet.populations['Albert'].get_data()["spikes"]
-    monalisa_spikes = spikenet.populations['MonaLisa'].get_data()["spikes"]
+    # monalisa_spikes = spikenet.populations['MonaLisa'].get_data()["spikes"]
     plt.step(np.arange(len(albert_spikes)), albert_spikes, alpha=0.3)
-    plt.step(np.arange(len(albert_spikes)), monalisa_spikes, alpha=0.3)
+    # plt.step(np.arange(len(albert_spikes)), monalisa_spikes, alpha=0.3)
     N = 10
     plt.plot(np.convolve(albert_spikes, np.ones(N)/N))
 
-    states = np.array(spikenet.populations['Albert'].get_data()['states'])
-    print(states.shape)
-    plt.figure(2)
-    for i in range(1):
-        plt.plot(states[:, i, 0],marker=".")
-        # print(states[:, i, 0])
-        plt.title("V")
-    plt.figure(3)
-    for i in range(5):
-        plt.plot(states[:, i, 1],marker=".")
-        plt.title("gsyn_exc")
+    # states = np.array(spikenet.populations['Albert'].get_data()['states'])
+    # print(states.shape)
+    # plt.figure(2)
+    # for i in range(1):
+    #     plt.plot(states[:, i, 0],marker=".")
+    #     # print(states[:, i, 0])
+    #     plt.title("V")
+    # plt.figure(3)
+    # for i in range(5):
+    #     plt.plot(states[:, i, 1],marker=".")
+    #     plt.title("gsyn_exc")
 
-    plt.figure(4)
-    for i in range(5):
-        plt.plot(states[:, i, 2],marker=".")
-        plt.title("gsyn_inh")
+    # plt.figure(4)
+    # for i in range(5):
+    #     plt.plot(states[:, i, 2],marker=".")
+    #     plt.title("gsyn_inh")
 
-    plt.figure(5)
-    for i in range(5):
-        plt.plot(states[:, i, 3],marker=".")
-        plt.title("u")
+    # plt.figure(5)
+    # for i in range(5):
+    #     plt.plot(states[:, i, 3],marker=".")
+    #     plt.title("u")
 
-    states = np.array(spikenet.populations['MonaLisa'].get_data()['states'])
+    # states = np.array(spikenet.populations['MonaLisa'].get_data()['states'])
 
-    plt.figure(6)
-    for idx, feat in enumerate(["V", "gsyne", "gsyni", "u"]):
-        plt.plot(states[:, 0, idx], label=feat)
-    plt.title("MonaLisa")
-    plt.legend()
+    # plt.figure(6)
+    # for idx, feat in enumerate(["V", "gsyne", "gsyni", "u"]):
+    #     plt.plot(states[:, 0, idx], label=feat)
+    # plt.title("MonaLisa")
+    # plt.legend()
 
 
     plt.show()
