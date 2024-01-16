a547e9ad043aabf13672c8a578af11bb5774aa31
djanloo <zzibecu@gmail.com>
2024-01-16 13:45:49 +0100

    minor

--- quilt/interface/base_objects.pyx
+++ quilt/interface/base_objects.pyx
@@ -12,14 +12,14 @@ cdef class ParaMap:
     def __cinit__(self, dict params):
         self._paramap = new cParaMap()
 
-        self.is_neuron_paramap = "neuron_type" not in params.keys()
-        self.is_oscillator_paramap = "oscillator_type" not in params.keys()
+        self.is_neuron_paramap = ("neuron_type" in params.keys())
+        self.is_oscillator_paramap = ("oscillator_type" in params.keys())
 
         if (not self.is_neuron_paramap) and (not self.is_oscillator_paramap):
             message = "ParaMap must have a 'neuron_type' or 'oscillator_type' field\n"
             message += f"Possible values are:\n"
             message += f"\tneuron_type: {list(NEURON_TYPES.keys())}"
-            # message += f"\oscillator_type: {list(OSCILLATOR_TYPES.keys())}"
+            # message += rf"\oscillator_type: {list(OSCILLATOR_TYPES.keys())}"
 
             raise KeyError(message)
         
