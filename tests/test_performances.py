6882df791fae404dd9be1a19b1c0ac9f9aa6cbb6
djanloo <zzibecu@gmail.com>
2024-02-04 00:52:20 +0100

    minor: file rename

--- tests/test_performances.py
+++ tests/test_performances.py
@@ -1,7 +1,7 @@
 from quilt.builder import SpikingNetwork, NeuronCatalogue
 
-nn = NeuronCatalogue.from_yaml("bg_analysis/basal_ganglia_neurons.yaml")
-sn = SpikingNetwork.from_yaml("bg_analysis/basal_ganglia_network.yaml", nn)
+nn = NeuronCatalogue.from_yaml("bg_analysis/ortone_neurons.yaml")
+sn = SpikingNetwork.from_yaml("bg_analysis/ortone_network.yaml", nn)
 
 sn.build()
 sn.run(dt=0.1, time=600)
