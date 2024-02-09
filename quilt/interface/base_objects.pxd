10ac1bc5c275dbd2ec317ffbbae44abf32346bab
djanloo <zzibecu@gmail.com>
2024-02-09 21:37:53 +0100

    updated Projections

--- quilt/interface/base_objects.pxd
+++ quilt/interface/base_objects.pxd
@@ -1,4 +1,5 @@
 from libcpp cimport bool
+from libcpp.vector cimport vector
 cimport quilt.interface.cinterface as cinter
 
 cdef class ParaMap:
@@ -7,3 +8,11 @@ cdef class ParaMap:
         bool is_neuron_paramap
         bool is_oscillator_paramap
         dict params_dict, converted_params_dict
+
+cdef class Projection:
+    cdef:
+        int start_dimension, end_dimension
+        vector[vector[float]] _weights
+        vector[vector[float]] _delays 
+        cinter.Projection * _projection
+        float [:,:] weights, delays
--- quilt/interface/base_objects.pyx
+++ quilt/interface/base_objects.pyx
@@ -1,12 +1,17 @@
 # distutils: language = c++
 import numpy as np
 cimport numpy as np
+import ctypes
+
+
+# from libc import free, malloc
+from libcpp cimport vector
 
 # TODO: solve relative import from quilt.interface import cinterface
 from quilt.interface.cinterface cimport ParaMap as cParaMap
 
 NEURON_TYPES = {"base_neuron":0, "aqif":1,"aqif2":2 ,"izhikevich":3, "aeif":4}
-
+OSCILLATOR_TYPES = {"base_oscillator":0, "harmonic": 1}
 cdef class ParaMap:
 
     def __cinit__(self, dict params):
@@ -21,7 +26,7 @@ cdef class ParaMap:
             message = "ParaMap must have a 'neuron_type' or 'oscillator_type' field\n"
             message += f"Possible values are:\n"
             message += f"\tneuron_type: {list(NEURON_TYPES.keys())}"
-            # message += rf"\oscillator_type: {list(OSCILLATOR_TYPES.keys())}"
+            message += rf"\oscillator_type: {list(OSCILLATOR_TYPES.keys())}"
 
             raise KeyError(message)
         
@@ -33,8 +38,8 @@ cdef class ParaMap:
             self.converted_params_dict['neuron_type'] = NEURON_TYPES[self.params_dict['neuron_type']]
             
         # Converts to unsigned int
-        # if "oscillator_type" in params_dict.keys():
-        #     params_dict['oscillator_type'] = OSCILLATOR_TYPES[params_dict['oscillator_type']]
+        if "oscillator_type" in self.params_dict.keys():
+            self.converted_params_dict['oscillator_type'] = OSCILLATOR_TYPES[self.params_dict['oscillator_type']]
     
         for key in self.converted_params_dict.keys():
             key_bytes = key.encode('utf-8') if isinstance(key, str) else key
@@ -46,3 +51,58 @@ cdef class ParaMap:
     
     def __reduce__(self):
         return (self.__class__, (self.params_dict,))
+
+
+cdef class Projection:
+    def __cinit__(self,  np.ndarray[np.float32_t, ndim=2,mode='c'] weights, np.ndarray[np.float32_t,ndim=2,mode='c'] delays):
+
+        self.weights = weights
+        self.delays = delays
+
+        self.start_dimension = weights.shape[0]
+        self.end_dimension   = weights.shape[1]
+
+        if delays.shape[0] != weights.shape[0]:
+            raise ValueError(f"Weights and delays have different start dimension: {weights.shape[0]} vs {delays.shape[0]}")
+        if delays.shape[1] != weights.shape[1]:
+            raise ValueError(f"Weights and delays have different end dimension: {weights.shape[1]} vs {delays.shape[1]}")
+
+        self._weights = vector[vector[float]](self.start_dimension)
+        self._delays = vector[vector[float]](self.start_dimension)
+
+        for i in range(self.start_dimension):
+            weights_row = vector[float](self.end_dimension)
+            delays_row = vector[float](self.end_dimension)
+
+            for j in range(self.end_dimension):
+
+                weights_row[j] = weights[i,j]
+                delays_row[j] = delays[i,j]
+
+            self._weights[i] = weights_row
+            self._delays[i] = delays_row
+        
+
+    @property
+    def weights(self):
+        return self.weights
+    
+    @property
+    def delays(self):
+        return self.delays
+
+    @property
+    def cweights(self):
+        return self._weights
+    
+    @property
+    def cdelays(self):
+        return self._delays
+    
+    # def __dealloc__(self):
+    #     if self._projection != NULL:
+    #         del self._projection
+    #     if self._weights != NULL:
+    #         free(self._weights)
+    #     if self._delays != NULL:
+    #         free(self._delays)
