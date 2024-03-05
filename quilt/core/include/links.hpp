a278169c4c7e438f63f62cd817e08def32ac4ba5
djanloo <zzibecu@gmail.com>
2024-03-05 13:02:39 +0100

    Link require a weight and a delay apart from other parameters

--- quilt/core/include/links.hpp
+++ quilt/core/include/links.hpp
@@ -5,110 +5,148 @@
 #include <stdexcept>
 #include <memory>
 
-
 using std::vector;
 using std::shared_ptr;
 
 class Oscillator;
 
-/*************************************************** LINK BASE ************************************************/
+/**
+ * @brief Base class for links between oscillators.
+ *
+ * This class serves as the base class for different link models connecting oscillators.
+ * It provides a common interface and structure for various link types.
+ */
+class Link {
+public:
+    shared_ptr<Oscillator> source; ///< The source oscillator of the link.
+    shared_ptr<Oscillator> target; ///< The target oscillator of the link.
+    ParaMap* params; ///< Parameters associated with the link.
+    float weight; ///< Weight of the link.
+    float delay; ///< Delay in the link.
 
-// NOTE: the linking strategy is still 'not elegant' (pronounced 'notto ereganto' with the voice of Housemaster Henry Henderson from Spy x Family)
-// 
-// My intent was finding a strategy to template & polymorph stuff
-// in order to reduce to the minimum value the number of templates in cython code.
+    /**
+     * @brief Constructor for the Link class.
+     * @param source The source oscillator.
+     * @param target The target oscillator.
+     * @param weight Weight of the link.
+     * @param delay Delay in the link.
+     * @param params Parameters associated with the link.
+     */
+    Link(shared_ptr<Oscillator> source, shared_ptr<Oscillator> target, float weight, float delay, ParaMap* params)
+        : source(source),
+          target(target),
+          weight(weight),
+          delay(delay),
+          params(params) {}
 
-// Clearly, the ideal minimum is 0.
-// I will consider myself satisfied only when this minimum is reached, i.e. 
-// when the code will deduce the right link function when just:
-// 
-// osc1.connect(osc2)
-// 
-// is called.
+    /**
+     * @brief Virtual function to get the value of the link.
+     * @param axis The axis for which to get the value.
+     * @param now Current time for the inner steps of Runge-Kutta.
+     * @return The value of the link.
+     */
+    virtual double get(int axis, double now) {
+        throw std::runtime_error("Using virtual `get()` of LinkBase");
+    };
+
+    /**
+     * @brief Set the evolution context for the link.
+     * @param evo EvolutionContext for the link.
+     */
+    void set_evolution_context(EvolutionContext* evo) {
+        this->evo = evo;
+    };
+
+protected:
+    EvolutionContext* evo; ///< EvolutionContext associated with the link.
+};
 
 /**
- * @brief Base class of links
- * 
-*/
-class Link{
-    public:
-        shared_ptr<Oscillator> source;
-        shared_ptr<Oscillator> target;
-        ParaMap * params;
-        float weight, delay;
+ * @brief Factory class for creating different link types.
+ */
+class LinkFactory {
+    typedef std::function<Link*(shared_ptr<Oscillator>, shared_ptr<Oscillator>, float, float, ParaMap*)> linker;
 
-        Link(shared_ptr<Oscillator> source, shared_ptr<Oscillator> target, ParaMap * params)
-            :   source(source),
-                target(target),
-                params(params)
-        {
-            // Must have at least the delay and the weight
-            // throw an error otherwise
-            if (!params->has("delay") || !params->has("weight"))
-                throw std::invalid_argument("Link must contain 'delay' and 'weight' parameters");
-            
-            if (params->get<float>("weight") == 0.0)
-            {
-                // No link-making procedure must arrive at this point
-                // zero-valued links must be treated upstream
-                throw std::invalid_argument("Initialized a zero-weighted link between two oscillators");
-            }
+public:
+    LinkFactory();
 
-            // Assign as class attributes for faster access
-            weight = params->get<float>("weight");
-            delay = params->get<float>("delay");
-        }
-        ~Link(){}
-        
-        virtual double get(int axis, double now) // Note: it needs `now` for the innner steps of RK 
-        {
-            throw runtime_error("Using virtual `get()` of LinkBase");
-        };
-        
-        void set_evolution_context(EvolutionContext * evo)
-        {
-            this->evo = evo;
-        };
-    protected:
-        EvolutionContext * evo;
+    /**
+     * @brief Add a linker function for creating a specific type of link.
+     * @param key Pair of oscillator types as a key.
+     * @param lker Linker function.
+     * @return True if the linker was added successfully, false otherwise.
+     */
+    bool add_linker(std::pair<std::string, std::string> const& key, linker const& lker) {
+        return _linker_map.insert(std::make_pair(key, lker)).second;
+    }
+
+    /**
+     * @brief Get a specific type of link using the provided parameters.
+     * @param source Source oscillator.
+     * @param target Target oscillator.
+     * @param weight Weight of the link.
+     * @param delay Delay in the link.
+     * @param params Parameters associated with the link.
+     * @return A pointer to the created link.
+     */
+    Link* get_link(shared_ptr<Oscillator> source, shared_ptr<Oscillator> target, float weight, float delay, ParaMap* params);
+
+private:
+    std::map<std::pair<std::string, std::string>, linker> _linker_map; ///< Map of oscillator type pairs to linker functions.
 };
 
-/*************************************** LINK FACTORY *************************************/
-// Builder method for Link-derived objects
-template <typename DERIVED>
-Link * link_maker(shared_ptr<Oscillator> source, shared_ptr<Oscillator> target, ParaMap * params)
-{
-    return new DERIVED(source, target, params);
-}
-
-class LinkFactory{
-    typedef std::function<Link*(shared_ptr<Oscillator>, shared_ptr<Oscillator>, ParaMap *)> linker;
-    public:
-        LinkFactory();
-        bool add_linker(std::pair<string, string> const& key, linker const& lker) {
-            return _linker_map.insert(std::make_pair(key, lker)).second;
-        }
-
-        Link * get_link(shared_ptr<Oscillator> source,shared_ptr<Oscillator> target, ParaMap * params);
-
-    private:
-        map<std::pair<string, string>, linker> _linker_map;
-};
-
-// Singleton method to return a unique instance of LinkFactory
+/**
+ * @brief Get a reference to the singleton instance of LinkFactory.
+ * @return Reference to the LinkFactory instance.
+ */
 LinkFactory& get_link_factory();
 
-/****************************************************** LINK MODELS ****************************************************/
-class JRJRLink : public Link{
-    public:
-        JRJRLink(shared_ptr<Oscillator> source, shared_ptr<Oscillator> target, ParaMap * params)
-            :   Link(source, target, params){}
-        double get(int axis, double now) override;
+/**
+ * @brief Derived class for a specific link model (JRJR).
+ */
+class JRJRLink : public Link {
+public:
+    /**
+     * @brief Constructor for JRJRLink.
+     * @param source Source oscillator.
+     * @param target Target oscillator.
+     * @param weight Weight of the link.
+     * @param delay Delay in the link.
+     * @param params Parameters associated with the link.
+     */
+    JRJRLink(shared_ptr<Oscillator> source, shared_ptr<Oscillator> target, float weight, float delay, ParaMap* params)
+        : Link(source, target, weight, delay, params) {}
+
+    /**
+     * @brief Get the value of the link for a specific axis at the given time.
+     * @param axis The axis for which to get the value.
+     * @param now Current time.
+     * @return The value of the link.
+     */
+    double get(int axis, double now) override;
 };
 
-class LJRLJRLink : public Link{
-    public:
-        LJRLJRLink(shared_ptr<Oscillator> source, shared_ptr<Oscillator> target, ParaMap * params)
-            :   Link(source, target, params){}
-        double get(int axis, double now) override;
+/**
+ * @brief Derived class for another specific link model (LJRLJR).
+ */
+class LJRLJRLink : public Link {
+public:
+    /**
+     * @brief Constructor for LJRLJRLink.
+     * @param source Source oscillator.
+     * @param target Target oscillator.
+     * @param weight Weight of the link.
+     * @param delay Delay in the link.
+     * @param params Parameters associated with the link.
+     */
+    LJRLJRLink(shared_ptr<Oscillator> source, shared_ptr<Oscillator> target, float weight, float delay, ParaMap* params)
+        : Link(source, target, weight, delay, params) {}
+
+    /**
+     * @brief Get the value of the link for a specific axis at the given time.
+     * @param axis The axis for which to get the value.
+     * @param now Current time.
+     * @return The value of the link.
+     */
+    double get(int axis, double now) override;
 };
