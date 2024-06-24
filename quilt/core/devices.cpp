d80f3e743bcf30dae181ca8d48e0ea59dc452dea
djanloo <zzibecu@gmail.com>
2024-06-24 17:28:51 +0200

    extended Cox window method #16

--- quilt/core/devices.cpp
+++ quilt/core/devices.cpp
@@ -6,7 +6,7 @@
 #include <stdexcept>
 #include <limits>
 
-#define N_THREADS_INHOM_POISS_INJECT 1
+#define N_THREADS_INHOM_POISS_INJECT 4
 
 /**
  * Disclaimer: this section is cumbersome due to the redundancy of the 'get_history()' methods.
@@ -154,7 +154,17 @@ InhomPoissonSpikeSource::InhomPoissonSpikeSource( Population * pop,
         currently_generated_time(0)
 {
     // Spike time initialization
-    next_spike_times = std::vector<double> (pop->n_neurons, 0);
+    integration_start = std::vector<double> (pop->n_neurons, 0);
+
+    // No integration leftovers at the beginning
+    integration_leftovers = std::vector<double> (pop->n_neurons, 0);
+
+    // Initializes stochastic integration thresholds
+    RNG rng;
+    integration_thresholds = std::vector<double> (pop->n_neurons, 0);
+    for (int i=0; i < pop->n_neurons; i++){
+        integration_thresholds[i] = -std::log(rng.get_uniform());
+    }
 
     // weights initialization
     weights = std::vector<float>(pop->n_neurons, 0);
@@ -211,12 +221,11 @@ Logger inhomlog("ihompoisson.log");
 void InhomPoissonSpikeSource::_inject_partition(double now, double dt, int start_id, int end_id, RNGDispatcher * rng_disp){
     
     // DECOMMENT IF THIS METHOD CREATES TROUBLE
-    // inhomlog.set_level(DEBUG);
+    // inhomlog.set_level(INFO);
 
-    double y; // The threshold for spike generation
-    double Y; // The cumulative variable 
     double avg_rate_in_timestep = 0; // This is for the trapezoidal rule and for checks
 
+    bool add_spike = false;
     int seek_buffer_index;
     int last_spike_time_index;  // Time index of the last produced spike
     int timestep_done;         // Timesteps of the integration done
@@ -230,19 +239,27 @@ void InhomPoissonSpikeSource::_inject_partition(double now, double dt, int start
     // Gets an independent random number generator
     RNG * thread_rng = rng_disp->get_rng();
 
+    inhomlog.log(INFO, "Starting generation " + to_string(generation));
+
     for (int i = start_id; i < end_id; i++)
     {
         // produced_spikes = vector<double>(0); //DEBUG
+        stringstream ss;
 
-        last_spike_time_index = static_cast<int>(next_spike_times[i]/dt);
-        inhomlog.log(INFO, "neuron " + to_string(i) + " started from the last spike at t = " + to_string(next_spike_times[i]));
+        ss << "Starting neuron " << i <<". Current state:"<< endl;
+        ss << "last_integration_extrema: " << integration_start[i] << endl;
+        ss << "integration_leftovers: "<< integration_leftovers[i] << endl;
+        ss << "integration threshold: "<< integration_thresholds[i] << endl; 
+
+        last_spike_time_index = static_cast<int>(integration_start[i]/dt);
+        inhomlog.log(DEBUG, ss.str());
 
         // If the neuron has a spike OVER this generation window, it must be skipped
-        if (next_spike_times[i] > currently_generated_time + generation_window_length){
-            inhomlog.log(INFO, "Neuron " + to_string(i) + " had a spike over the generation window");
-            inhomlog.log(INFO, "\tcurrently_generated_time:" + std::to_string(currently_generated_time));
-            inhomlog.log(INFO, "\tgeneration window:" + std::to_string(generation_window_length));
-            inhomlog.log(INFO, "\tlast spike time:" + std::to_string(next_spike_times[i]));
+        if (integration_start[i] > currently_generated_time + generation_window_length){
+            inhomlog.log(DEBUG, "Neuron " + to_string(i) + " had a spike over the generation window");
+            inhomlog.log(DEBUG, "\tcurrently_generated_time:" + std::to_string(currently_generated_time));
+            inhomlog.log(DEBUG, "\tgeneration window:" + std::to_string(generation_window_length));
+            inhomlog.log(DEBUG, "\tlast spike time:" + std::to_string(integration_start[i]));
 
             // Skip the neuron, the appropriate generation will take care of him
             continue;
@@ -262,14 +279,17 @@ void InhomPoissonSpikeSource::_inject_partition(double now, double dt, int start
          * This will raise a "Spike in past" error. 
         */
         do{
-            y = -std::log(thread_rng->get_uniform());
-            Y = 0; 
             timestep_done = 0;
-
-            inhomlog.log(DEBUG, "Integration of r(t) started at t = " + to_string((last_spike_time_index + timestep_done)*dt));
+            stringstream ss;
+            ss << "neuron " << i <<  ": integration of r(t) started at t = " << (last_spike_time_index + timestep_done)*dt;
+            inhomlog.log(DEBUG, ss.str());
             
             // This loop goes on until the integral of the rate overcomes the exp-distributed random variable y
-            while (Y <= y){
+            // OR if the loop reaches the end of the buffer.
+            // In the first case a spike is added.
+            // In the latter case the integration extrema is set to the end of the window, another generation will take care of this
+
+            while (true){
 
                 // Remember that, for each generation, r(t) ~ rate_buffer[<int>((t-currently_generated_time)/dt)]
                 // So the rate when the last spike happened is
@@ -277,19 +297,12 @@ void InhomPoissonSpikeSource::_inject_partition(double now, double dt, int start
                 // so to carry out the integration from last_spike_time you have to request
                 // r(last_spike_time + timesteps_done*dt) = rate_buffer[<int>((last_spike_time - currently_generated_time)/dt ) + timesteps_done]
                 seek_buffer_index = last_spike_time_index + timestep_done - static_cast<int>(currently_generated_time/dt);
-                if (seek_buffer_index >= rate_function_buffer.size()){
-                    stringstream msg;
-                    msg <<"InhomPoisson is integrating outside the rate function buffer: accessing element "
-                        << seek_buffer_index << " while length is " << rate_function_buffer.size();
-                    throw runtime_error(msg.str());
+                if (seek_buffer_index >= rate_function_buffer.size()){        
+                    // Exits with no spike generated but with modified integration extrema
+                    add_spike = false;
+                    break;
                 }
-                // BEFORE INTRODUCING RATE FUNCTION BUFFER
-                // avg_rate_in_timestep = 0.5*(
-                //                             rate_function( (last_spike_time_index + timestep_done)*dt) + \
-                //                             rate_function( (last_spike_time_index + timestep_done + 1)*dt)\
-                //                            );
 
-                // AFTER INTRODUCING RATE FUNCTION BUFFER
                 avg_rate_in_timestep = 0.5*(
                                             rate_function_buffer[seek_buffer_index]     +   \
                                             rate_function_buffer[seek_buffer_index + 1]     \
@@ -315,37 +328,74 @@ void InhomPoissonSpikeSource::_inject_partition(double now, double dt, int start
                 avg_rate_in_timestep /= 1e3;
 
                 // Trapezoidal rule
-                Y += avg_rate_in_timestep * dt;                
+                integration_leftovers[i] += avg_rate_in_timestep * dt;                
 
                 timestep_done ++;
+
+                if(integration_leftovers[i] >= integration_thresholds[i]){
+                    add_spike = true;
+                    break;
+                }
             }
 
-            // At this point Y has overcomed y
-            proposed_next_spike_time_index = last_spike_time_index + timestep_done;
-            proposed_next_spike_time = proposed_next_spike_time_index * dt;
+            if (add_spike){
 
-            // This is real bad
-            if (proposed_next_spike_time < now){
-                string msg = "A spike was produced in the past from InhomogeneousPoissonSS.\n";
-                msg+= "now : " + to_string(now) + "\n";
-                msg += "neuron: " + to_string(i) + "\n";
-                msg += "last spike produced: " + to_string(next_spike_times[i]) + "\n";
-                msg += "proposed spike: " + to_string(proposed_next_spike_time) + "\n";
-                inhomlog.log(ERROR, msg);
-                throw runtime_error(msg);
+                // At this point Y has overcomed y
+                proposed_next_spike_time_index = last_spike_time_index + timestep_done;
+                proposed_next_spike_time = proposed_next_spike_time_index * dt;
+
+                // This is real bad
+                if (proposed_next_spike_time < now){
+                    string msg = "A spike was produced in the past from InhomogeneousPoissonSS.\n";
+                    msg+= "now : " + to_string(now) + "\n";
+                    msg += "neuron: " + to_string(i) + "\n";
+                    msg += "last spike produced: " + to_string(integration_start[i]) + "\n";
+                    msg += "proposed spike: " + to_string(proposed_next_spike_time) + "\n";
+                    inhomlog.log(ERROR, msg);
+                    throw runtime_error(msg);
+                }
+                
+                // Accepts the spike and set the integration extrema as the last produced spike
+                last_spike_time_index = proposed_next_spike_time_index;
+                integration_start[i] = proposed_next_spike_time;
+
+                // produced_spikes.push_back(proposed_next_spike_time); // DEBUG
+
+                // Adds a spike to the neuron's queue
+                pop->neurons[i]->incoming_spikes.emplace(this->weights[i], proposed_next_spike_time);
+                outfile.write( to_string(i) +  " "  + to_string(proposed_next_spike_time)); // DEBUG
+                generated_spikes++;
+
+                // Resets threshold and integration leftover
+                integration_thresholds[i] = -std::log(thread_rng->get_uniform());
+                integration_leftovers[i] = 0.0;
+
+                stringstream ss;
+                ss << "Emitted a spike at t = " << proposed_next_spike_time;
+                inhomlog.log(DEBUG, ss.str());
+
+            }else{
+
+                // Tells the next generation that for the i-th neuron the integration
+                // was already carried out up to the end of the window
+                integration_start[i] = currently_generated_time + generation_window_length;
+
+                stringstream ss;
+                ss << "Window ended without spike emission. Setting integration extrema for neuron " << i << " as " << integration_start[i] 
+                    << " (currently generated time: " << currently_generated_time << " and window: "<< generation_window_length << " )";
+                inhomlog.log(DEBUG, ss.str());
             }
-            
-            last_spike_time_index = proposed_next_spike_time_index;
-            next_spike_times[i] = proposed_next_spike_time;
 
-            // produced_spikes.push_back(proposed_next_spike_time); // DEBUG
 
-            // Adds a spike to the neuron's queue
-            pop->neurons[i]->incoming_spikes.emplace(this->weights[i], next_spike_times[i]);
-            outfile.write( to_string(i) +  " "  + to_string(next_spike_times[i])); // DEBUG
-            generated_spikes++; 
+        } while (integration_start[i] < currently_generated_time + generation_window_length);
 
-        } while (proposed_next_spike_time < currently_generated_time + generation_window_length);
+        ss.str("");ss.clear();
+
+        ss << "Ended neuron " << i <<". Final state:"<< endl;
+        ss << "last_integration_extrema: " << integration_start[i] << endl;
+        ss << "integration_leftovers: "<< integration_leftovers[i] << endl;
+        ss << "integration threshold: "<< integration_thresholds[i] << endl; 
+        inhomlog.log(DEBUG, ss.str());
     } 
 
     // Frees the RNG dispatcher
@@ -366,17 +416,14 @@ void InhomPoissonSpikeSource::inject(EvolutionContext * evo){
         ss <<  "InhomPoiss is injecting - now is " << evo->now << " with dt "<<evo->dt; 
         get_global_logger().log(INFO,ss.str());
 
-        inhomlog.log(INFO, "Before this generation, " + std::to_string(nullcalls) + " null calls were performed");
-        inhomlog.log(INFO, "\tnow:" + std::to_string(evo->now));
-        inhomlog.log(INFO, "\tcurrently_generated_time:" + std::to_string(currently_generated_time));
-        inhomlog.log(INFO, "Starting generation " + std::to_string(generation) + " - [ " + to_string(currently_generated_time) + " -> " + to_string(currently_generated_time + generation_window_length) + "]");
+        inhomlog.log(DEBUG, "Before this generation, " + std::to_string(nullcalls) + " null calls were performed");
+        inhomlog.log(DEBUG, "\tnow:" + std::to_string(evo->now));
+        inhomlog.log(DEBUG, "\tcurrently_generated_time:" + std::to_string(currently_generated_time));
+        inhomlog.log(DEBUG, "Starting generation " + std::to_string(generation) + " - [ " + to_string(currently_generated_time) + " -> " + to_string(currently_generated_time + generation_window_length) + "]");
     }
 
     // Evaluates the rate function and stores it in the buffer
-    // Since the generation is stopped when a spike is in the next window
-    // the required length of the buffer is surely more than one window
-    // and surely less than 3
-    int rate_func_buf_size = static_cast<int>(2*generation_window_length/evo->dt);
+    int rate_func_buf_size = static_cast<int>(generation_window_length/evo->dt);
     rate_function_buffer = vector<double>(rate_func_buf_size, -1.0);
 
     // Copies the discretised rate function. The interval that will be used is [now, now + 2*generation_window_length]
@@ -385,9 +432,9 @@ void InhomPoissonSpikeSource::inject(EvolutionContext * evo){
     // For each generation, r(t) ~ rate_buf[<int>((t-currently_generated_time)/dt)]
     for (int i = 0; i < rate_func_buf_size; i++){
         rate_function_buffer[i] = rate_function(currently_generated_time + i*evo->dt);
-        cout << rate_function_buffer[i] << " ";
+        // cout << rate_function_buffer[i] << " ";
     }
-    cout << endl;
+    // cout << endl;
     
     int n_threads = N_THREADS_INHOM_POISS_INJECT;
     if (pop->n_neurons < 50){
@@ -401,7 +448,7 @@ void InhomPoissonSpikeSource::inject(EvolutionContext * evo){
     RNGDispatcher rng_dispatcher(n_threads);
 
     for (int i=0; i < n_threads; i++){
-        inhomlog.log(INFO, "starting thread " + to_string(i) + " || neurons [" + to_string(i*pop->n_neurons/n_threads) + "," + to_string((i+1)*pop->n_neurons/n_threads-1));
+        inhomlog.log(DEBUG, "starting thread " + to_string(i) + " || neurons [" + to_string(i*pop->n_neurons/n_threads) + "," + to_string((i+1)*pop->n_neurons/n_threads-1));
         threads.emplace_back(&InhomPoissonSpikeSource::_inject_partition, this ,
                             evo->now, evo->dt,
                             i*pop->n_neurons/n_threads, (i+1)*pop->n_neurons/n_threads-1,
