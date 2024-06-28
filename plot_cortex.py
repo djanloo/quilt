46cac4b0419851cf84452901878c997c74c0924e
djanloo <zzibecu@gmail.com>
2024-06-28 17:48:40 +0200

    solved bug #20

--- plot_cortex.py
+++ plot_cortex.py
@@ -1,16 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+dT = 2
+dt = 0.05
 
 osc_vars = np.loadtxt("cortex.txt")
 N_oscill = osc_vars.shape[1]
 
 for i in range(N_oscill):
-    plt.plot(osc_vars[:, i], marker=".")
+    plt.plot(np.arange(len(osc_vars[:, i]))*dT, osc_vars[:, i], marker=".")
 
 osc_vars_interp = np.loadtxt("cortex_interpolated.txt")
 print(osc_vars_interp)
 for i in range(N_oscill):
-    plt.plot(np.arange(len(osc_vars_interp[:, i])) *0.1, osc_vars_interp[:, i], marker=".", ms=4)
+    plt.plot(np.arange(len(osc_vars_interp[:, i])) *dt, osc_vars_interp[:, i], marker=".", ms=4)
 
 plt.show()
