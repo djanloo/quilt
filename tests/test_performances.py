7e25a867598899671b5a4a9f0c435260bd784919
djanloo <zzibecu@gmail.com>
2024-07-12 12:42:23 +0200

    added check for activity in test performances

--- tests/test_performances.py
+++ tests/test_performances.py
@@ -2,5 +2,11 @@ from quilt.builder import SpikingNetwork, NeuronCatalogue
 
 sn = SpikingNetwork.from_yaml("bg_analysis/ortone_network_dispersive.yaml", "bg_analysis/ortone_neurons.yaml")
 
+sn.monitorize_spikes()
 sn.build()
-sn.run(dt=0.1, time=600)
+sn.run(dt=0.1, time=60)
+
+import matplotlib.pyplot as plt
+
+plt.plot(sn.populations["D1"].get_data('spikes'))
+plt.show()
