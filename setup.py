a4ec9d493cefcf34eda50b4a7c08b06db35961de
djanloo <zzibecu@gmail.com>
2024-07-16 11:30:38 +0200

    this config roughly works, maybe smth is redundant

--- setup.py
+++ setup.py
@@ -1,65 +1,20 @@
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
-from Cython.Build import cythonize
-from Cython.Compiler.Options import get_directive_defaults
-import numpy as np
 import os
+from setuptools import setup
+from setuptools.command.build_ext import build_ext as _build_ext
 
-# Path to the compiled library
-LIBRARY_PATH = "libquilt.so"
+class build_ext(_build_ext):
+    def run(self):
+        os.system('meson setup builddir')
+        os.system('ninja -C builddir')
+        super().run()
 
-# Package
-PACKAGE_DIR = "./quilt"
-
-# Base configs
-BIN_FOLDER = './interface'
-CYTHON_GEN_FOLDER = './cython_generated'
-DEFAULT_INCLUDES = ["./", 
-                    "./quilt", 
-                    "./core/include", 
-                    np.get_include()]
-
-# Cartella corrente
-old_dir = os.getcwd()
-os.chdir(PACKAGE_DIR)
-
-extension_names = ["base", "spiking", "oscill", "multiscale"]
-extension_common_kwargs = dict( include_dirs=DEFAULT_INCLUDES + ["core/include"],
-                                language="c++",
-                                extra_compile_args=["-O3", "-std=c++17"],
-                                define_macros= [('NPY_NO_DEPRECATED_API','NPY_1_7_API_VERSION')], #Silences npy deprecated warn
-                                extra_link_args=["-std=c++17", LIBRARY_PATH])
-
-extensions = []
-for ext_name in extension_names:
-    extensions += [Extension(ext_name, sources=[f"{BIN_FOLDER}/{ext_name}.pyx"], **extension_common_kwargs)]
-
-
-cython_compiler_directives = get_directive_defaults()
-cython_compiler_directives['language_level'] = "3"
-cython_compiler_directives['warn'] = True
-
-# Compiles the extensions
-ext_modules = cythonize(
-    extensions,
-    nthreads=4,
-    include_path=["."],
-    build_dir=CYTHON_GEN_FOLDER,
-    force=False,
-    annotate=False,
-    compiler_directives=cython_compiler_directives
-)
-
-# Configurazione di setup.py
 setup(
     name='quilt',
-    packages=['quilt'],
-    cmdclass={"build_ext": build_ext},
-    include_dirs=DEFAULT_INCLUDES,
-    ext_modules=ext_modules,
-    script_args=["build_ext", f"--build-lib=./{BIN_FOLDER}"],
-    options={"build_ext": {"inplace": False, "force": True, "parallel": True}},
+    version='0.1.0',
+    description='Descrizione del mio progetto',
+    author='Tuo Nome',
+    author_email='tuo@email.com',
+    ext_modules=[],
+    cmdclass={'build_ext': build_ext},
+    zip_safe=False,
 )
-
-# Goes back to the root folder
-os.chdir(old_dir)
